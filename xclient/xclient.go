0c65abfae8d38199be830006b4347a691054ba1c
bruce0hh <bruce0hh@gmail.com>
2022-09-04 13:38:35 +0800

    feat:服务发现广播

--- xclient/xclient.go
+++ xclient/xclient.go
@@ -4,17 +4,18 @@ import (
 	"context"
 	"gorpc"
 	"io"
+	"reflect"
 	"sync"
 )
 
 /*
- 支持负载均衡的客户端XCLIENT
+ 支持负载均衡的客户端XCLIENT，重新包装client
 */
 
 type XClient struct {
-	discovery Discovery
-	mode      SelectMode
-	opt       *gorpc.Option
+	discovery Discovery     // 服务发现实例
+	mode      SelectMode    // 负载均衡模式
+	opt       *gorpc.Option // 协议选项
 	mu        sync.Mutex
 	clients   map[string]*gorpc.Client
 }
@@ -68,4 +69,47 @@ func (x *XClient) Call(ctx context.Context, serviceMethod string, args, reply in
 		return err
 	}
 	return x.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
+
+// Broadcast 广播：如果任意一个服务实例发生错误，则返回其中一个错误；如果调用成功，则返回其中一个结果
+func (x *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	servers, err := x.discovery.GetAll()
+	if err != nil {
+		return err
+	}
+
+	var (
+		wg sync.WaitGroup
+		mu sync.Mutex
+		e  error
+	)
+
+	replyDone := reply == nil
+	ctx, cancel := context.WithCancel(ctx)
+	// 为了提升性能，请求是并发的
+	for _, rpcAddr := range servers {
+		wg.Add(1)
+		go func(rpcAddr string) {
+			defer wg.Done()
+
+			var clonedReply interface{}
+			if reply != nil {
+				clonedReply = reflect.New(reflect.ValueOf(reply).Elem().Type()).Interface()
+			}
+			err := x.call(rpcAddr, ctx, serviceMethod, args, clonedReply)
+			// 使用互斥锁波阿虎增error和reply能被正确赋值
+			mu.Lock()
+			if err != nil && e == nil {
+				e = err
+				cancel() // 借助cancel，确保有错误发生时，能快速失败
+			}
+			if err == nil && !replyDone {
+				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(clonedReply).Elem())
+				replyDone = true
+			}
+			mu.Unlock()
+		}(rpcAddr)
+	}
+	wg.Wait()
+	return e
 }
