3453d5dae97787fac7de8e417107ffe2e9d8e8bb
Bruce0hh <bruce0hh@gmail.com>
2023-02-11 17:53:16 +0800

    feat：ADD lru add/len method

--- go-cache/lru/lru.go
+++ go-cache/lru/lru.go
@@ -56,3 +56,26 @@ func (c *Cache) RemoveOldest() {
 		}
 	}
 }
+
+// Add 新增/修改
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok { // 如果键存在，则更新对应节点的值，并将该节点移到队尾
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else { // 如果键不存在，则添加新节点，并在字典中添加key和value
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	// 更新c.nbytes，如果超过了设定的最大值，则移除最少访问的节点
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+// Len 获取缓存数
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
